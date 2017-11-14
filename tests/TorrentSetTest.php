b58864370417832e6a767e200ec3aabb76f87a61
pxgamer <owzie123@gmail.com>
2017-11-14 22:09:30 +0000

    Added tests for Torrent set() methods

---
+++ tests/TorrentSetTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace pxgamer\YTS;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class TorrentSetTest
+ */
+class TorrentSetTest extends TestCase
+{
+    /**
+     * @throws \Exception
+     */
+    public function testCanGetUrl()
+    {
+        $movie = new Torrent();
+        $movie->setUrl('');
+        $this->assertEquals('', $movie->getUrl());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanGetHash()
+    {
+        $movie = new Torrent();
+        $movie->setHash('');
+        $this->assertEquals('', $movie->getHash());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanGetQuality()
+    {
+        $movie = new Torrent();
+        $movie->setQuality('');
+        $this->assertEquals('', $movie->getQuality());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanGetSeeds()
+    {
+        $movie = new Torrent();
+        $movie->setSeeds(1);
+        $this->assertEquals(1, $movie->getSeeds());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanGetPeers()
+    {
+        $movie = new Torrent();
+        $movie->setPeers(1);
+        $this->assertEquals(1, $movie->getPeers());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanGetSize()
+    {
+        $movie = new Torrent();
+        $movie->setSize('');
+        $this->assertEquals('', $movie->getSize());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanGetSizeBytes()
+    {
+        $movie = new Torrent();
+        $movie->setSizeBytes(1);
+        $this->assertEquals(1, $movie->getSizeBytes());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanGetDateUploaded()
+    {
+        $movie = new Torrent();
+        $movie->setDateUploaded('');
+        $this->assertEquals('', $movie->getDateUploaded());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanGetDateUploadedUnix()
+    {
+        $time = time();
+        $movie = new Torrent();
+        $movie->setDateUploadedUnix($time);
+        $this->assertEquals($time, $movie->getDateUploadedUnix());
+    }
+}
