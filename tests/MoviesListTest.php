9f2485e97f0325a536b442be7cb766b9559b36a4
pxgamer <owzie123@gmail.com>
2017-11-14 21:07:51 +0000

    Added tests for the Movies::list() method

---
+++ tests/MoviesListTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace pxgamer\YTS;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class MoviesListTest
+ */
+class MoviesListTest extends TestCase
+{
+    /**
+     * @throws \Exception
+     */
+    public function testCanRetrieveMovies()
+    {
+        $movies = Movies::list();
+        $this->assertTrue($movies->isNotEmpty());
+        $this->assertTrue($movies->first()->getId() > 0);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanRetrieveMoviesByQuality()
+    {
+        $movies = Movies::list([
+            'quality' => Movies::QUALITY_720
+        ]);
+        $this->assertTrue($movies->isNotEmpty());
+        $this->assertTrue($movies->first()->getId() > 0);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanRetrieveMoviesByQuery()
+    {
+        $movies = Movies::list([
+            'query_term' => 'Lord of the Rings',
+        ]);
+        $this->assertTrue($movies->isNotEmpty());
+        $this->assertTrue($movies->first()->getId() > 0);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanRetrieveMoviesByPage()
+    {
+        $movies = Movies::list([
+            'page' => 2,
+        ]);
+        $this->assertTrue($movies->isNotEmpty());
+        $this->assertTrue($movies->first()->getId() > 0);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanRetrieveMoviesByMinimumRating()
+    {
+        $movies = Movies::list([
+            'minimum_rating' => 5,
+        ]);
+        $this->assertTrue($movies->isNotEmpty());
+        $this->assertTrue($movies->first()->getId() > 0);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanRetrieveMoviesByGenre()
+    {
+        $movies = Movies::list([
+            'genre' => 'Action',
+        ]);
+        $this->assertTrue($movies->isNotEmpty());
+        $this->assertTrue($movies->first()->getId() > 0);
+    }
+}
