51c380541515de8bea7141bcfd7d98f9ffee4651
pxgamer <owzie123@gmail.com>
2017-11-14 21:17:15 +0000

    Added tests for the Movies::suggestions() method

---
+++ tests/MoviesSuggestionsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace pxgamer\YTS;
+
+use PHPUnit\Framework\TestCase;
+use pxgamer\YTS\Exceptions\NoDataFoundException;
+
+/**
+ * Class MoviesSuggestionsTest
+ */
+class MoviesSuggestionsTest extends TestCase
+{
+    /**
+     * @throws \Exception
+     */
+    public function testCanRetrieveMovie()
+    {
+        $movies = Movies::suggestions([
+            'movie_id' => 10,
+        ]);
+        $this->assertTrue($movies->isNotEmpty());
+        $this->assertTrue($movies->first()->getId() > 0);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanRetrieveMovieWithImages()
+    {
+        $movies = Movies::suggestions([
+            'movie_id'    => 10,
+        ]);
+        $this->assertTrue($movies->isNotEmpty());
+        $this->assertTrue($movies->first()->getId() > 0);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanRetrieveMovieWithCast()
+    {
+        $movies = Movies::suggestions([
+            'movie_id'  => 10,
+        ]);
+        $this->assertTrue($movies->isNotEmpty());
+        $this->assertTrue($movies->first()->getId() > 0);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testThrowsErrorOnInvalidData()
+    {
+        $this->expectException(NoDataFoundException::class);
+        Movies::suggestions();
+    }
+}
