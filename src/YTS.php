0350b44f732b195efb8ab637240933e36e0bcc09
pxgamer <owzie123@gmail.com>
2019-03-21 11:31:36 +0000

    Update main YTS class

--- src/YTS.php
+++ src/YTS.php
@@ -2,42 +2,15 @@
 
 namespace pxgamer\YTS;
 
-use GuzzleHttp\Client;
+use pxgamer\YTS\Adapter\HttpAdapter;
 
 class YTS
 {
-    /** The current version of the API. */
-    public const API_VERSION = 2;
-    /** The base uri of the API. */
-    public const BASE_URI = 'https://yts.am/api/v'.self::API_VERSION.'/';
+    /** @var HttpAdapter */
+    protected $adapter;
 
-    /** @var Client */
-    private static $client;
-
-    /* Retrieve the array response from the API endpoint. */
-    public static function getFromApi(string $endpoint, array $parameters = null): ?array
+    public function __construct(HttpAdapter $adapter)
     {
-        $parameters = $parameters ?? [];
-
-        $response = self::getClient()
-            ->get($endpoint, ['query' => $parameters]);
-
-        return \GuzzleHttp\json_decode(
-            $response->getBody()
-                ->getContents(),
-            true
-        );
-    }
-
-    /* Retrieve the current GuzzleHttp Client instance. */
-    private static function getClient(): Client
-    {
-        if (!self::$client instanceof Client) {
-            self::$client = new Client([
-                'base_uri' => self::BASE_URI,
-            ]);
-        }
-
-        return self::$client;
+        $this->adapter = $adapter;
     }
 }
